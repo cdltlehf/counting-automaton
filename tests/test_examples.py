8b0df65104f294c20392109edd2e624b51d18832
river-martin <rivermartin128@gmail.com>
2025-04-14 18:06:57 +0200

    refactor: separate execution logic from `main` in `computation_info.py` for reusability; add tests that run examples

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -12,7 +12,7 @@ import cai4py.counting_automaton.super_config as sc
 class TestPositionCountingAutomaton(unittest.TestCase):
     """Unit tests for position_counter_automaton.py"""
 
-    def setUp(self) -> None:
+    def setUp(self):
         logging.basicConfig(level=logging.INFO)
         warnings.simplefilter(action="ignore", category=FutureWarning)
         self.maxDiff = None  # pylint: disable=invalid-name
@@ -20,5 +20,23 @@ class TestPositionCountingAutomaton(unittest.TestCase):
         self.test_cases = load_test_cases(dataset_path)
         self.timeout = 1
 
-    def test_super_config(self) -> None:
+    def test_super_config(self):
         run_and_log_trace(sc.SuperConfig, self.test_cases)
+
+    def test_bounded_super_config(self):
+        run_and_log_trace(sc.BoundedSuperConfig, self.test_cases)
+
+    def test_counter_config(self):
+        run_and_log_trace(sc.CounterConfig, self.test_cases)
+
+    def test_bounded_counter_config(self):
+        run_and_log_trace(sc.BoundedCounterConfig, self.test_cases)
+
+    def test_sparse_counter_config(self):
+        run_and_log_trace(sc.SparseCounterConfig, self.test_cases)
+
+    def test_determinized_counter_config(self):
+        run_and_log_trace(sc.DeterminizedCounterConfig, self.test_cases)
+
+    def test_determinized_bounded_counter_config(self):
+        run_and_log_trace(sc.DeterminizedBoundedCounterConfig, self.test_cases)
