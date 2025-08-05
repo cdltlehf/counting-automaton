582fc6212b134b7bffe6162bdc44bc191f8d296f
river-martin <rivermartin128@gmail.com>
2025-08-05 13:17:12 +0200

    Remove try except

--- src/cai4py/parser_tools/re.py
+++ src/cai4py/parser_tools/re.py
@@ -1,13 +1,7 @@
 """_re.py"""
 
-try:
-    from re.compile import compile as _compile  # type: ignore
-    from re.parser import State  # type: ignore
-    from re.parser import SubPattern
-
-except ImportError:
-    from sre_compile import compile as _compile
-    from sre_parse import State
-    from sre_parse import SubPattern
+from re._compiler import compile as _compile
+from re._parser import State
+from re._parser import SubPattern
 
 __all__ = ["_compile", "State", "SubPattern"]
