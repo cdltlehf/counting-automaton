ee0721640be045a97b0766ba07b3474fd02edccb
river-martin <129603538+river-martin@users.noreply.github.com>
2025-11-04 12:18:26 +0200

    Add memory measurement scripts for DFA and Polyglot, enhancing output organization

--- src/scripts/measure_memory.sh
+++ src/scripts/measure_memory.sh
@@ -30,3 +30,36 @@ python -O -m cai4py.instrumentation.measure_memory \
 	--expansion-type outer \
 	--input-encoding "utf-8" \
 	--num-strings-per-regex 30
+
+mkdir -p outputs/dfa
+# SNORT3
+python -O -m cai4py.instrumentation.measure_memory \
+	--random-string-dir ../regex-input-gen/generated/counters/snort3-random-strings \
+	--regex-file ../../re-datasets/snort3/c-patterns-filtered.txt \
+	--log-file outputs/dfa/inner-snort3-peak-mem-usage.tsv \
+	--expansion-type inner \
+	--input-encoding "utf-8" \
+	--num-strings-per-regex 30
+python -O -m cai4py.instrumentation.measure_memory \
+	--random-string-dir ../regex-input-gen/generated/counters/snort3-random-strings \
+	--regex-file ../../re-datasets/snort3/c-patterns-filtered.txt \
+	--log-file outputs/dfa/outer-snort3-peak-mem-usage.tsv \
+	--expansion-type outer \
+	--input-encoding "utf-8" \
+	--num-strings-per-regex 30
+
+# Polyglot
+python -O -m cai4py.instrumentation.measure_memory \
+	--random-string-dir ../regex-input-gen/generated/counters/polyglot-random-strings \
+	--regex-file ../../re-datasets/polyglot/c-patterns-filtered.txt \
+	--log-file outputs/dfa/inner-polyglot-peak-mem-usage.tsv \
+	--expansion-type inner \
+	--input-encoding "utf-8" \
+	--num-strings-per-regex 30
+python -O -m cai4py.instrumentation.measure_memory \
+	--random-string-dir ../regex-input-gen/generated/counters/polyglot-random-strings \
+	--regex-file ../../re-datasets/polyglot/c-patterns-filtered.txt \
+	--log-file outputs/dfa/outer-polyglot-peak-mem-usage.tsv \
+	--expansion-type outer \
+	--input-encoding "utf-8" \
+	--num-strings-per-regex 30
