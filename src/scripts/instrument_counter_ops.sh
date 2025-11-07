1d7eb5006d927c0ffb52ff7ebdc6587f92e02d80
river-martin <129603538+river-martin@users.noreply.github.com>
2025-11-07 13:57:55 +0200

    Refactor output directory structure and update file paths in instrumentation scripts for Snort3 and Polyglot

--- src/scripts/instrument_counter_ops.sh
+++ src/scripts/instrument_counter_ops.sh
@@ -1,18 +1,19 @@
-mkdir -p outputs/csa
+mkdir -p outputs/csa/snort3/inner
+mkdir -p outputs/csa/polyglot/inner
 python -O -m cai4py.instrumentation.instrument_counter_ops \
 	--random-string-dir ../regex-input-gen/generated/counters/snort3-random-strings \
-	--regex-file ../../re-datasets/snort3/c-patterns-filtered.txt \
+	--regex-file ../snort3/c-patterns-filtered.txt \
 	--num-strings-per-regex 30 \
-	--op-counts-output outputs/csa/op-name-to-counts-snort3.tsv \
-	--merge-sizes-output outputs/csa/merge-sizes-snort3.npy \
-	--clone-sizes-output outputs/csa/clone-sizes-snort3.npy \
+	--op-counts-output ../outputs/snort3/inner/csa/op-name-to-counts-snort3.tsv \
+	--merge-sizes-output ../outputs/snort3/inner/csa/merge-sizes-snort3.npy \
+	--clone-sizes-output ../outputs/snort3/inner/csa/clone-sizes-snort3.npy \
 	--expansion-type inner
 python -O -m cai4py.instrumentation.instrument_counter_ops \
 	--random-string-dir ../regex-input-gen/generated/counters/polyglot-random-strings \
-	--regex-file ../../re-datasets/polyglot/c-patterns-filtered.txt \
+	--regex-file ../polyglot/c-patterns-filtered.txt \
 	--num-strings-per-regex 30 \
-	--op-counts-output outputs/csa/op-name-to-counts-polyglot.tsv \
-	--merge-sizes-output outputs/csa/merge-sizes-polyglot.npy \
-	--clone-sizes-output outputs/csa/clone-sizes-polyglot.npy \
+	--op-counts-output ../outputs/polyglot/inner/csa/op-name-to-counts-polyglot.tsv \
+	--merge-sizes-output ../outputs/polyglot/inner/csa/merge-sizes-polyglot.npy \
+	--clone-sizes-output ../outputs/polyglot/inner/csa/clone-sizes-polyglot.npy \
 	--expansion-type inner
 # TODO: run with attack strings as well?
--- src/scripts/measure_memory.sh
+++ src/scripts/measure_memory.sh
@@ -1,65 +1,40 @@
-mkdir -p outputs/csa
+mkdir -p outputs/csa/inner/polyglot
+mkdir -p outputs/csa/inner/snort3
+
 # SNORT3
 python -O -m cai4py.instrumentation.measure_memory \
-	--random-string-dir ../regex-input-gen/generated/counters/snort3-random-strings \
-	--regex-file ../../re-datasets/snort3/c-patterns-filtered.txt \
-	--log-file outputs/csa/inner-snort3-peak-mem-usage.tsv \
+	--random-string-dir generated/counters/snort3-random-strings \
+	--regex-file ../snort3/c-patterns-filtered.txt \
+	--log-file outputs/csa/snort3/inner/peak-mem-usage.tsv \
 	--expansion-type inner \
-	--input-encoding "utf-8" \
-	--num-strings-per-regex 30
-python -O -m cai4py.instrumentation.measure_memory \
-	--random-string-dir ../regex-input-gen/generated/counters/snort3-random-strings \
-	--regex-file ../../re-datasets/snort3/c-patterns-filtered.txt \
-	--log-file outputs/csa/outer-snort3-peak-mem-usage.tsv \
-	--expansion-type outer \
 	--input-encoding "utf-8" \
 	--num-strings-per-regex 30
 
 # Polyglot
 python -O -m cai4py.instrumentation.measure_memory \
-	--random-string-dir ../regex-input-gen/generated/counters/polyglot-random-strings \
-	--regex-file ../../re-datasets/polyglot/c-patterns-filtered.txt \
-	--log-file outputs/csa/inner-polyglot-peak-mem-usage.tsv \
+	--random-string-dir generated/counters/polyglot-random-strings \
+	--regex-file ../polyglot/c-patterns-filtered.txt \
+	--log-file outputs/csa/polyglot/inner/peak-mem-usage.tsv \
 	--expansion-type inner \
-	--input-encoding "utf-8" \
-	--num-strings-per-regex 30
-python -O -m cai4py.instrumentation.measure_memory \
-	--random-string-dir ../regex-input-gen/generated/counters/polyglot-random-strings \
-	--regex-file ../../re-datasets/polyglot/c-patterns-filtered.txt \
-	--log-file outputs/csa/outer-polyglot-peak-mem-usage.tsv \
-	--expansion-type outer \
 	--input-encoding "utf-8" \
 	--num-strings-per-regex 30
 
-mkdir -p outputs/dfa
-# SNORT3
+mkdir -p outputs/dfa/snort3/inner
+mkdir -p outputs/dfa/polyglot/inner
+
 python -O -m cai4py.instrumentation.measure_memory \
-	--random-string-dir ../regex-input-gen/generated/counters/snort3-random-strings \
-	--regex-file ../../re-datasets/snort3/c-patterns-filtered.txt \
-	--log-file outputs/dfa/inner-snort3-peak-mem-usage.tsv \
+	--random-string-dir generated/counters/snort3-random-strings \
+	--regex-file ../snort3/c-patterns-filtered.txt \
+	--log-file outputs/dfa/snort3/inner/peak-mem-usage.tsv \
 	--expansion-type inner \
-	--input-encoding "utf-8" \
-	--num-strings-per-regex 30
-python -O -m cai4py.instrumentation.measure_memory \
-	--random-string-dir ../regex-input-gen/generated/counters/snort3-random-strings \
-	--regex-file ../../re-datasets/snort3/c-patterns-filtered.txt \
-	--log-file outputs/dfa/outer-snort3-peak-mem-usage.tsv \
-	--expansion-type outer \
 	--input-encoding "utf-8" \
 	--num-strings-per-regex 30
 
 # Polyglot
 python -O -m cai4py.instrumentation.measure_memory \
-	--random-string-dir ../regex-input-gen/generated/counters/polyglot-random-strings \
-	--regex-file ../../re-datasets/polyglot/c-patterns-filtered.txt \
-	--log-file outputs/dfa/inner-polyglot-peak-mem-usage.tsv \
+	--random-string-dir generated/counters/polyglot-random-strings \
+	--regex-file ../polyglot/c-patterns-filtered.txt \
+	--log-file outputs/dfa/polyglot/inner/peak-mem-usage.tsv \
 	--expansion-type inner \
-	--input-encoding "utf-8" \
-	--num-strings-per-regex 30
-python -O -m cai4py.instrumentation.measure_memory \
-	--random-string-dir ../regex-input-gen/generated/counters/polyglot-random-strings \
-	--regex-file ../../re-datasets/polyglot/c-patterns-filtered.txt \
-	--log-file outputs/dfa/outer-polyglot-peak-mem-usage.tsv \
-	--expansion-type outer \
 	--input-encoding "utf-8" \
 	--num-strings-per-regex 30
