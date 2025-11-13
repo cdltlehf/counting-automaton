f01542241cc37e4f8a4e3f0132847bc0821dd667
river-martin <129603538+river-martin@users.noreply.github.com>
2025-11-13 18:29:54 +0200

    Update timing log file paths in time_random.sh for Snort3 and Polyglot; add missing full expansion cases and adjust cache types for consistency.

--- src/scripts/time_random.sh
+++ src/scripts/time_random.sh
@@ -4,6 +4,7 @@ mkdir -p outputs/csa/snort3/outer
 mkdir -p outputs/csa/polyglot/outer
 mkdir -p outputs/csa/snort3/full
 mkdir -p outputs/csa/polyglot/full
+# No cache
 # SNORT3
 # Inner
 python -O -m cai4py.instrumentation.time_random \
@@ -32,9 +33,8 @@ python -O -m cai4py.instrumentation.time_random \
 	--input-encoding "utf-8" \
 	--num-strings-per-regex 30 \
 	--cache-type "none"
-
-
 # Polyglot
+# Inner
 python -O -m cai4py.instrumentation.time_random \
 	--random-string-dir generated/counters/polyglot-random-strings \
 	--regex-file polyglot/c-patterns-filtered.txt \
@@ -43,39 +43,60 @@ python -O -m cai4py.instrumentation.time_random \
 	--input-encoding "utf-8" \
 	--num-strings-per-regex 30 \
 	--cache-type "none"
+# Outer
 python -O -m cai4py.instrumentation.time_random \
 	--random-string-dir generated/counters/polyglot-random-strings \
 	--regex-file polyglot/c-patterns-filtered.txt \
-	--timing-log-file outputs/csa/polyglot/outer/random-throughputs-flush-on-full-cache.tsv \
+	--timing-log-file outputs/csa/polyglot/outer/random-throughputs.tsv \
 	--expansion-type outer \
 	--input-encoding "utf-8" \
 	--num-strings-per-regex 30 \
 	--cache-type "none"
+# Full
+python -O -m cai4py.instrumentation.time_random \
+	--random-string-dir generated/counters/polyglot-random-strings \
+	--regex-file polyglot/c-patterns-filtered.txt \
+	--timing-log-file outputs/csa/polyglot/full/random-throughputs.tsv \
+	--expansion-type full \
+	--input-encoding "utf-8" \
+	--num-strings-per-regex 30 \
+	--cache-type "none"
 
+# Flush on full cache
 # SNORT3
+# Inner
 python -O -m cai4py.instrumentation.time_random \
 	--random-string-dir generated/counters/snort3-random-strings \
 	--regex-file snort3/c-patterns-filtered.txt \
-	--timing-log-file outputs/csa/snort3/inner/random-throughputs-flush-on-full-cache.tsv \
+	--timing-log-file outputs/csa/snort3/inner/random-throughputs.tsv \
 	--expansion-type inner \
 	--input-encoding "utf-8" \
 	--num-strings-per-regex 30 \
 	--cache-type "flush_on_full"
+# Outer
 python -O -m cai4py.instrumentation.time_random \
 	--random-string-dir generated/counters/snort3-random-strings \
 	--regex-file snort3/c-patterns-filtered.txt \
-	--timing-log-file outputs/csa/snort3/outer/random-throughputs-flush-on-full-cache.tsv \
+	--timing-log-file outputs/csa/snort3/outer/random-throughputs.tsv \
 	--expansion-type outer \
 	--input-encoding "utf-8" \
 	--num-strings-per-regex 30 \
 	--cache-type "flush_on_full"
-
+# Full
+python -O -m cai4py.instrumentation.time_random \
+	--random-string-dir generated/counters/snort3-random-strings \
+	--regex-file snort3/c-patterns-filtered.txt \
+	--timing-log-file outputs/csa/snort3/full/random-throughputs.tsv \
+	--expansion-type full \
+	--input-encoding "utf-8" \
+	--num-strings-per-regex 30 \
+	--cache-type "flush_on_full"
 # Polyglot
 # Inner
 python -O -m cai4py.instrumentation.time_random \
 	--random-string-dir generated/counters/polyglot-random-strings \
 	--regex-file polyglot/c-patterns-filtered.txt \
-	--timing-log-file outputs/csa/polyglot/inner/random-throughputs-flush-on-full-cache.tsv \
+	--timing-log-file outputs/csa/polyglot/inner/random-throughputs.tsv \
 	--expansion-type inner \
 	--input-encoding "utf-8" \
 	--num-strings-per-regex 30 \
@@ -84,17 +105,75 @@ python -O -m cai4py.instrumentation.time_random \
 python -O -m cai4py.instrumentation.time_random \
 	--random-string-dir generated/counters/polyglot-random-strings \
 	--regex-file polyglot/c-patterns-filtered.txt \
-	--timing-log-file outputs/csa/polyglot/outer/random-throughputs-flush-on-full-cache.tsv \
+	--timing-log-file outputs/csa/polyglot/outer/random-throughputs.tsv \
 	--expansion-type outer \
 	--input-encoding "utf-8" \
 	--num-strings-per-regex 30 \
 	--cache-type "flush_on_full"
 # Full
 python -O -m cai4py.instrumentation.time_random \
-	--random-string-dir generated/counters/snort3-random-strings \
+	--random-string-dir generated/counters/polyglot-random-strings \
 	--regex-file polyglot/c-patterns-filtered.txt \
 	--timing-log-file outputs/csa/polyglot/full/random-throughputs.tsv \
 	--expansion-type full \
 	--input-encoding "utf-8" \
 	--num-strings-per-regex 30 \
-	--cache-type "none"
+	--cache-type "flush_on_full"
+
+# LRU cache
+# SNORT3
+# Inner
+python -O -m cai4py.instrumentation.time_random \
+	--random-string-dir generated/counters/snort3-random-strings \
+	--regex-file snort3/c-patterns-filtered.txt \
+	--timing-log-file outputs/csa/snort3/inner/random-throughputs.tsv \
+	--expansion-type inner \
+	--input-encoding "utf-8" \
+	--num-strings-per-regex 30 \
+	--cache-type "lru"
+# Outer
+python -O -m cai4py.instrumentation.time_random \
+	--random-string-dir generated/counters/snort3-random-strings \
+	--regex-file snort3/c-patterns-filtered.txt \
+	--timing-log-file outputs/csa/snort3/outer/random-throughputs.tsv \
+	--expansion-type outer \
+	--input-encoding "utf-8" \
+	--num-strings-per-regex 30 \
+	--cache-type "lru"
+# Full
+python -O -m cai4py.instrumentation.time_random \
+	--random-string-dir generated/counters/snort3-random-strings \
+	--regex-file snort3/c-patterns-filtered.txt \
+	--timing-log-file outputs/csa/snort3/full/random-throughputs.tsv \
+	--expansion-type full \
+	--input-encoding "utf-8" \
+	--num-strings-per-regex 30 \
+	--cache-type "lru"
+# Polyglot
+# Inner
+python -O -m cai4py.instrumentation.time_random \
+	--random-string-dir generated/counters/polyglot-random-strings \
+	--regex-file polyglot/c-patterns-filtered.txt \
+	--timing-log-file outputs/csa/polyglot/inner/random-throughputs.tsv \
+	--expansion-type inner \
+	--input-encoding "utf-8" \
+	--num-strings-per-regex 30 \
+	--cache-type "lru"
+# Outer
+python -O -m cai4py.instrumentation.time_random \
+	--random-string-dir generated/counters/polyglot-random-strings \
+	--regex-file polyglot/c-patterns-filtered.txt \
+	--timing-log-file outputs/csa/polyglot/outer/random-throughputs.tsv \
+	--expansion-type outer \
+	--input-encoding "utf-8" \
+	--num-strings-per-regex 30 \
+	--cache-type "lru"
+# Full
+python -O -m cai4py.instrumentation.time_random \
+	--random-string-dir generated/counters/polyglot-random-strings \
+	--regex-file polyglot/c-patterns-filtered.txt \
+	--timing-log-file outputs/csa/polyglot/full/random-throughputs.tsv \
+	--expansion-type full \
+	--input-encoding "utf-8" \
+	--num-strings-per-regex 30 \
+	--cache-type "lru"
